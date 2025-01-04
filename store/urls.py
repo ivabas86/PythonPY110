42bbc184d7a1fa1b0a8b1f5588fd89f8d99f878e
ivabas86 <iv.al.basov@gmail.com>
2025-01-04 12:42:53 +0300

    first

--- store/urls.py
+++ store/urls.py
@@ -5,10 +5,11 @@ from store.views import cart_view,cart_add_view,cart_del_view
 app_name = 'store'
 urlpatterns = [
     path('product/', product_),
-    path('', shop_view),
+    path('', shop_view, name='shop_view'),
     path('product/<slug:page>.html', product_page_view, name = "product_page_view"),
     path('product/<int:page>', product_page_view),
     path('cart/', cart_view),
     path('cart/add/<str:id_product>', cart_add_view),
     path('cart/del/<str:id_product>', cart_del_view),
+
 ]
--- store/views.py
+++ store/views.py
@@ -28,31 +28,52 @@ def product_(request):
 
 def shop_view(request):
     if request.method == 'GET':
-        # with open('store/shop.html', encoding= 'utf-8') as f:
-        #     data = f.read()
-        # return HttpResponse(data)
-        return render(request, 'store/shop.html', context= {'products': DATABASE.values()})
+        id_ = request.GET.get("id")
+        if id_:
+            if id_ in DATABASE:
+                return JsonResponse(DATABASE.get(id_), json_dumps_params={'ensure_ascii': False, 'indent': 4})
+            return HttpResponseNotFound('Данного продукта нет в базе данных')
+        category_key = request.GET.get("category")
+        ordering_key = request.GET.get('ordering')
+        reverse_key = request.GET.get('reverse')
+        if ordering_key:
+            if str(reverse_key).lower() == 'true':
+                data = filtering_category(DATABASE, category_key, ordering_key, True)
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key)
+        else:
+            data = filtering_category(DATABASE, category_key)
+        return render(request, 'store/shop.html', context= {'products': data, 'category': category_key})
 
 def product_page_view(request, page):
     if request.method == 'GET':
         if isinstance (page, str):
             for prod in DATABASE.values():
                 if prod ['html'] == page:
-                    with open (f'store/products/{page}.html', encoding= 'utf-8') as f:
-                        data = f.read()
-                    return HttpResponse(data)
+                    # with open (f'store/products/{page}.html', encoding= 'utf-8') as f:
+                    #     data = f.read()
+                    return render(request, "store/product.html", context={"product": prod})
         elif isinstance(page, int):
             prod = DATABASE.get(str(page))
             if prod:
-                with open(f'store/products/{prod["html"]}.html', encoding= 'utf-8') as f:
-                    data = f.read()
-                return HttpResponse(data)
-
+                # with open(f'store/products/{prod["html"]}.html', encoding= 'utf-8') as f:
+                #     data = f.read()
+                return render(request, "store/product.html", context={"product": prod})
+        return HttpResponse(status=404)
 def cart_view(request):
     if request.method == "GET":
         data = view_in_cart()  # TODO Вызвать ответственную за это действие функцию
-        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})
+        if request.GET.get('format') == 'json':
+            return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                         'indent': 4})
+        products = []  # Список продуктов
+        for id_product, count in data.get('products').items():
+            product = DATABASE[id_product]  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
+            product['count'] = count # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
+            product['price_total'] = round(count * product['price_after'],2)  # добавление общей цены позиции с ограничением в 2 знака
+            # 3. добавьте product в список products
+            products.append(product)
+        return render(request, "store/cart.html", context={"products": products})
 
 def cart_add_view(request, id_product):
     if request.method == "GET":
